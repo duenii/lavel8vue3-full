96db3213900825187acbaed0c569b3029a622271
duenii <nantanee.012@gmail.com>
2023-03-28 16:21:39 +0700

    first commit

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'image',
+        'user_id'
+    ];
+
+    /**
+     * Relationship to Users
+     */
+    public function user()
+    {
+        //select * from product
+        return $this->belongsTo(User::class);
+    }
+    public function users(){
+
+        // SELECT * 
+        // FROM products
+        // INNER JOIN users
+        // ON products.user_id = users.id;
+
+        return $this->belongsTo('App\Models\User','user_id')->select(['id','fullname','avatar']); 
+    }
+}
